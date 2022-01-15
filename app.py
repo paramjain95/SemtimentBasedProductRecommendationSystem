e17dbdd29c7f7a03dfb0abc4c57866139ccea6b5
Param Jain <78639882+paramjain95@users.noreply.github.com>
2022-01-15 12:51:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ from flask import Flask, render_template, request
 from model import recommend_product, get_all_username
 
 app = Flask(__name__)  # intitialize the flaks app  # common 
-
+all_users = get_all_username()
 @app.route('/', methods = ['POST', 'GET'])
 def home():
     data=[]
     user=""
     if request.method == 'POST':
         user = request.form["userid"]
-        if user in get_all_username():
+
+        if user in all_users:
             data=recommend_product(user)
     return render_template("index.html", len=len(data),products=data, user=user,flag=False)
 
 
 @app.route('/userList', methods = ['GET','POST'])
 def userList():
-    data=get_all_username()
+    data=all_users
     return render_template("index.html", len=len(data), products=None, user=data,flag=True)
 
 
