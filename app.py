18fa3e65894cde88183cc950f1ac7d4ff9dd37e8
Param Jain <78639882+paramjain95@users.noreply.github.com>
2022-01-15 12:58:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,16 +9,17 @@ def home():
     user=""
     if request.method == 'POST':
         user = request.form["userid"]
-
         if user in all_users:
             data=recommend_product(user)
-    return render_template("index.html", len=len(data),products=data, user=user,flag=False)
+        else:
+            return render_template("index.html", len=len(data), products=data, user=user, flag=False, flag1=True)
+    return render_template("index.html", len=len(data),products=data, user=user,flag=False,flag1=False)
 
 
 @app.route('/userList', methods = ['GET','POST'])
 def userList():
     data=all_users
-    return render_template("index.html", len=len(data), products=None, user=data,flag=True)
+    return render_template("index.html", len=len(data), products=None, user=data,flag=True,flag1=False)
 
 
 if __name__ == '__main__' :
